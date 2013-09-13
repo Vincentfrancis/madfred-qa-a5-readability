22f56c115808d81bac671094bb843770035a4012
Vincentfrancis <student@192.168.0.16>
2013-09-13 12:51:50 -0400

    Assn. 5

--- src/DamageThisOne.java
+++ src/DamageThisOne.java
@@ -10,33 +10,37 @@ public class DamageThisOne {
 	 */
 	
 	public static void main(String[] args) {
-		//This section declares variables and instantiates the Scanner sc
+		//This section declares something
 		int ticketNumber, stem, checkDigit;
-		Scanner sc = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 		
-		/* This section requests input from the user, in the form of a 
-		 * six digit number, then it generates a stem and checkdigit 
-		 * from that number
+		/* This section requests numbers from the scanner
 		 */
 		System.out.print("Please enter a six-digit ticket number: ");
-		ticketNumber = sc.nextInt();
+			ticketNumber = sc.nextInt();
+			
+			
+			
 		stem = ticketNumber / 10;
-		checkDigit = ticketNumber % 10;	
+			checkDigit = ticketNumber % 10;	
 
-		/* This section will check the validity of the ticket number
-		 * and continue to prompt the user for a new ticket number
-		 * while the current number is invalid
+		/* This section calls the scanner
 		 */
 		while (checkDigit != stem % 7) {
-			System.out.println("That is NOT a valid ticket number, bucko!");
+				System.out.println("That is NOT a valid ticket number, bucko!");
 			System.out.print("Try again: ");
-			ticketNumber = sc.nextInt();
+			
+			
+				ticketNumber = sc.nextInt();
+				
+				
 			stem = ticketNumber / 10;
-			checkDigit = ticketNumber % 10;	
+			
+				checkDigit = ticketNumber % 10;	
 		}
 						
-		System.out.println("That is a valid ticket number, well done!");
-	
+					System.out.println("That is a valid ticket number, well done!");
+	// this is the end of the program
 	}
 
 }
